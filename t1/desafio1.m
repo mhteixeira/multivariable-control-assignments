03321aec6e40b07168ae41c2f278d2fc9c134256
Murillo Teixeira <murillo.teixeira@usp.br>
2022-04-09 17:23:46 -0300

    Estrutura de solução para o Desafio 1 criada

---
+++ t1/desafio1.m
@@ -0,0 +1,32 @@
+%% Enunciado do Desafio 1
+%  
+% # Calcular o Gramiano de Controlabilidade do sistema massa-mola proposto
+% # Implementar um diagrama do Simulink que leve o sistema de x_0 a um x_f
+% # Simular o sistema usando o diagrama e apresentar resultados
+% # Alterar as massas e repetir a simulação
+% # Discutir os resultados obtidos
+
+format short;
+close all;
+clear; clc;
+
+%% Definição dos parâmetros do problema
+M   = 1;
+K   = 1;
+T_f = 1;
+
+x_0 = [0 0 0 0];
+x_f = [1 0 1 0];
+
+%% Definição do sistema a ser controlado
+
+A   = [0 1 0 0; -K/M 0 -K/M 0; 0 0 0 1; -K/M 0 -K/M 0];
+B   = [0 0; 1/M 0; 0 0; 0 1/M];
+C   = [1 0 0 0; 0 0 1 0];
+D   = [0 0; 0 0];
+
+%% Cálculo do gramiano
+
+%% Execução do diagrama do Simulink
+
+%% Apresentação dos resultados
