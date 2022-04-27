704227ce337f4a784bb6a0c0af902b99b019edbe
Murillo Teixeira <murillo.teixeira@usp.br>
2022-04-26 22:33:26 -0300

    T1D1: Lógica de aumentar a massa e fazer os gráficos implementada

--- t1/d1/ctrb_input.m
+++ t1/d1/ctrb_input.m
@@ -1,7 +1,11 @@
-% function u = ctrb_input(t, A, B, x0, xf, Wc, Tf)
-%     u = -B.'*exp(Tf-t);
-function u = ctrb_input(t)
-    global A  B x0 xf Wc Tf;
-    u = -B.'*expm((Tf-t)*A.')*inv(Wc)*(expm(Tf*A)*x0-xf);
+function [u1, u2] = ctrb_input(sim_step, Tf, A, B, x0, xf, Wc)
+    u = [];
+    tsim = (0:sim_step:Tf);
+    for i = 1:(Tf/sim_step+1)
+        t = tsim(i);
+        u = [u -B.'*expm((Tf-t)*A.')*inv(Wc)*(expm(Tf*A)*x0-xf)];
+    end
+    u1 = [tsim; u(1, :)]'; 
+    u2 = [tsim; u(2, :)]';
 end
 
--- t1/d1/desafio1.m
+++ t1/d1/desafio1.m
@@ -9,14 +9,16 @@
 format short;
 close all;
 clear; clc;
-global A  B x0 xf Wc Tf;
+global A  B x0 xf Wc Tf sim_step;
+
 %% Definição dos parâmetros do problema
 M   = 1;
 K   = 1;
-Tf  = 1;
+Tf  = 2;
+sim_step = 0.01;
 
 x0 = [0; 0; 0; 0];
-xf = [1; 0; 2; 10];
+xf = [1; 0; 1; 0];
 
 %% Definição do sistema a ser controlado
 
@@ -29,32 +31,78 @@ D   = [0 0; 0 0;0 0;0 0];
 
 Wc = ctrb_gramm(A, B, [0 Tf]);
 
+%% Esforço de controle em malha aberta
+
+[u1, u2] = ctrb_input(sim_step, Tf, A, B, x0, xf, Wc);
+
 %% Execução do diagrama do Simulink
 
-simout = sim("crtb_model.slx", 'TimeOut', Tf);
-u2  = simout.u2;
-u1  = simout.u1;
-x1  = simout.x1;
-x2  = simout.x2;
-x1dot  = simout.x1dot;
-x2dot  = simout.x2dot;
-t   = simout.tout;
+sim_out = sim("crtb_model.slx", 'StartTime', '0', 'StopTime', num2str(Tf), 'FixedStep', num2str(sim_step));
+x1      = sim_out.x1;
+x2      = sim_out.x2;
+x1dot   = sim_out.x1dot;
+x2dot   = sim_out.x2dot;
+t       = sim_out.tout;
  
-%% Apresentação dos resultados
+%% Apresentação dos resultados ideais
 
-figure(1);
+fig = figure('visible','off');
+set(fig, 'Position',  [0, 0, 800, 600])
+
 xlim([0, Tf]);
 subplot(2, 2, 1);
-plot(t, x1, 'b', t, x2, 'r');
+plot(t, x1, 'b', t, x2, 'r--');
 title('Curva de controle de posição');
-legend('$x_1$', '$x_2$','Interpreter','latex', 'FontSize',10);
+legend('$x_1$', '$x_2$','Interpreter','latex', 'FontSize',10, 'Location', 'Best');
 
 subplot(2, 2, 2);
-plot(t, x1dot, 'b', t, x2dot, 'r');
+plot(t, x1dot, 'b', t, x2dot, 'r--');
 title('Curva de controle de velocidade');
-legend('$\dot{x}_1$', '$\dot{x}_2$','Interpreter','latex', 'FontSize',10);
+legend('$\dot{x}_1$', '$\dot{x}_2$','Interpreter','latex', 'FontSize', 10, 'Location', 'Best');
 
 subplot(2, 1, 2);
-plot(t, u1, 'b', t, u2, 'r');
+plot(t, u1(:, 2), 'b', t, u2(:, 2), 'r--');
 title('Esforço de controle');
-legend('$u_1$', '$u_2$','Interpreter','latex', 'FontSize',10);
+legend('$u_1$', '$u_2$','Interpreter','latex', 'FontSize', 10, 'Location', 'Best');
+
+saveas(fig,'outputs/initial_result.pdf')
+close(fig)
+
+%% Mudança de parâmetros para alterar a massa 
+
+M   = M*1.1;
+A   = [0 1 0 0; -K/M 0 K/M 0; 0 0 0 1; K/M 0 -K/M 0];
+B   = [0 0; 1/M 0; 0 0; 0 1/M];
+
+%% Execução do diagrama do Simulink p/ massa alterada
+
+sim_out = sim("crtb_model.slx", 'StartTime', '0', 'StopTime', num2str(Tf), 'FixedStep', num2str(sim_step));
+x1      = sim_out.x1;
+x2      = sim_out.x2;
+x1dot   = sim_out.x1dot;
+x2dot   = sim_out.x2dot;
+t       = sim_out.tout;
+
+%% Apresentação dos resultados p/ massa alterada
+
+fig = figure('visible','off');
+set(fig, 'Position',  [0, 0, 800, 600])
+
+xlim([0, Tf]);
+subplot(2, 2, 1);
+plot(t, x1, 'b', t, x2, 'r--');
+title('Curva de controle de posição');
+legend('$x_1$', '$x_2$','Interpreter','latex', 'FontSize',10, 'Location', 'Best');
+
+subplot(2, 2, 2);
+plot(t, x1dot, 'b', t, x2dot, 'r--');
+title('Curva de controle de velocidade');
+legend('$\dot{x}_1$', '$\dot{x}_2$','Interpreter','latex', 'FontSize',10, 'Location', 'Best');
+
+subplot(2, 1, 2);
+plot(t, u1(:, 2), 'b', t, u2(:, 2), 'r--');
+title('Esforço de controle');
+legend('$u_1$', '$u_2$','Interpreter','latex', 'FontSize',10, 'Location', 'Best');
+
+saveas(fig,'outputs/altered_mass_result.pdf')
+close(fig)
