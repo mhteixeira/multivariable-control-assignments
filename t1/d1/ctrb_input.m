8b45a35c7fa8b660e25d7c37d4bc0b79ad7c326e
Murillo Teixeira <murillo.teixeira@usp.br>
2022-04-27 20:25:42 -0300

    T1D1: Método de calcular o controle foi simplificado

--- t1/d1/ctrb_input.m
+++ t1/d1/ctrb_input.m
@@ -1,11 +1,12 @@
 function [u1, u2] = ctrb_input(sim_step, Tf, A, B, x0, xf, Wc)
-    u = [];
     tsim = (0:sim_step:Tf);
-    for i = 1:(Tf/sim_step+1)
+    n = Tf/sim_step+1;
+    u1 = zeros(n, 2); u2 = zeros(n, 2);
+    for i = 1:n
         t = tsim(i);
-        u = [u -B.'*expm((Tf-t)*A.')*inv(Wc)*(expm(Tf*A)*x0-xf)];
+        u_i = -B.'*expm((Tf-t)*A.')/(Wc)*(expm(Tf*A)*x0-xf);
+        u1(i, :) = [t u_i(1)];
+        u2(i, :) = [t u_i(2)];
     end
-    u1 = [tsim; u(1, :)]'; 
-    u2 = [tsim; u(2, :)]';
 end
 
--- t1/d1/desafio1.m
+++ t1/d1/desafio1.m
@@ -18,14 +18,14 @@ Tf  = 2;
 sim_step = 0.01;
 
 x0 = [0; 0; 0; 0];
-xf = [1; 0; 10; 1];
+xf = [1; 0; 1; 0];
 
 %% Definição do sistema a ser controlado
 
 A   = [0 1 0 0; -K/M 0 K/M 0; 0 0 0 1; K/M 0 -K/M 0];
 B   = [0 0; 1/M 0; 0 0; 0 1/M];
 C   = eye(4);
-D   = [0 0; 0 0;0 0;0 0];
+D   = [0 0; 0 0; 0 0; 0 0];
 
 %% Cálculo do gramiano
 
