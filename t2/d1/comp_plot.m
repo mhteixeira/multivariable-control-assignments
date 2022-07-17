eed3823b9bc60d8d6874137fbb0768a2755236fd
Vinicius Meneses <54123152+0ViniciusSouza@users.noreply.github.com>
2022-07-17 13:18:30 -0300

    T2D1: Novos gráficos e simulação de sistema com rastreamento ao degrau
    - Gráficos adicionados:
        - y(t) com ruído.
        - Controle LQR.
        - Imposição de Polos com valores grandes.

--- t2/d1/comp_plot.m
+++ t2/d1/comp_plot.m
@@ -5,6 +5,10 @@ function comp_plot(sim_out, r, fig_name)
     x2      = sim_out.x2;
     x1dot   = sim_out.x1dot;
     x2dot   = sim_out.x2dot;
+    y1n     = sim_out.y1n;
+    y2n     = sim_out.y2n;
+    y1      = sim_out.y1;
+    y2      = sim_out.y2;
     t       = sim_out.tout;
     xf      = [r(1);0;r(2);0];
 
@@ -17,19 +21,37 @@ function comp_plot(sim_out, r, fig_name)
     fig = figure('visible','off');
     set(fig, 'Position',  [0, 0, 800, 600]);
     
-    subplot(2, 2, 1);
-    p=plot(t, x1, t, x2);
+    subplot(3, 2, 1);
+    p=plot(t, y1, t, y2);
     ylabel('Posição (m)');
     xlabel('Tempo (s)');
     xlim([0, t(end)]);
-    ylim([1.2 * min([xf(1), xf(3), min(x1), min(x2)])-0.1, ...
-        1.2 * max([xf(1), xf(3), max(x1), max(x2)])]+0.1);
+    ylim([1.2 * min([xf(1), xf(3), min(y1), min(y2)])-0.1, ...
+        1.2 * max([xf(1), xf(3), max(y1), max(y2)])]+0.1);
+    r1=yline(r(1),'--');
+    r1.Color = '#9999FF';
+    r2=yline(r(2),'--');
+    r2.Color = '#FF9999';
     title('Curva de controle de posição');
-    legend('$x_1$', '$x_2$','Interpreter','latex', 'FontSize',10, 'Location', 'Best');
+    legend('$y_1$', '$y_2$','Interpreter','latex', 'FontSize',10, 'Location', 'Best');
 %     datatip(p,'DataIndex',length(x1))
 %     datatip(p,'DataIndex',length(x2))
     
-    subplot(2, 2, 2);
+    subplot(3, 2, 2);
+    p=plot(t, y1n, t, y2n);
+    ylabel('Posição (m)');
+    xlabel('Tempo (s)');
+    r1=yline(r(1),'--');
+    r1.Color = '#9999FF';
+    r2=yline(r(2),'--');
+    r2.Color = '#FF9999';
+    xlim([0, t(end)]);
+    ylim([1.2 * min([xf(1), xf(3), min(y1n), min(y2n)])-0.1, ...
+        1.2 * max([xf(1), xf(3), max(y1n), max(y2n)])]+0.1);
+    title('Curva de controle de posição');
+    legend('$y_1 + n$', '$y_2 + n$','Interpreter','latex', 'FontSize',10, 'Location', 'Best');
+    
+    subplot(3, 1, 2);
     p=plot(t, x1dot,  t, x2dot);
     ylabel('Velocidade (m/s^2)');
     xlabel('Tempo (s)');
@@ -41,7 +63,7 @@ function comp_plot(sim_out, r, fig_name)
 %     datatip(p,'DataIndex',length(x1dot))
 %     datatip(p,'DataIndex',length(x2dot))
     
-    subplot(2, 1, 2);
+    subplot(3, 1, 3);
     p=plot(t, u1, t, u2);
     title('Esforço de controle');
     ylabel('Força de controle (N)');
--- t2/d1/desafio1.m
+++ t2/d1/desafio1.m
@@ -15,11 +15,11 @@ clear; clc;
 
 M   = 1;
 K_m   = 1;
-Tf  = 10;
+Tf  = 50;
 sim_step = 0.01;
 
 x0 = [0; 0; 0; 0];
-r = [1; 1];
+r = [1; 5];
 
 %% Definição do sistema a ser controlado
 
@@ -29,22 +29,89 @@ C   = [1 0 0 0; 0 0 1 0];
 D   = [0 0; 0 0];
 
 %% Imposição de polos:
-pF = [-4 -2 -4 -2];
+
+pF = [-5 -2 -5 -2];
 pK = [-1 -2 -1 -2];
 F = -place(A , B, pF);
 K = place(A.',C.',pK)';
 
-
-%% Simulação:
+%% Simulação com Imposição de Polos via place:
 
 sim_out = sim("comp_model.slx", ...
     'StartTime', '0', ...
     'StopTime', num2str(Tf), ...
     'FixedStep', num2str(sim_step));
 
-comp_plot(sim_out, r, 'initial_result.pdf');
+comp_plot(sim_out, r, 'place_result.pdf');
 
 %% Controle LQR:
 
 F = -lqr(A,B,eye(4),eye(2),0);
 K = lqr(A.',C.',eye(4),eye(2),0)';
+
+%% Simulação com controle LQR:
+
+sim_out2 = sim("comp_model.slx", ...
+    'StartTime', '0', ...
+    'StopTime', num2str(Tf), ...
+    'FixedStep', num2str(sim_step));
+
+comp_plot(sim_out2, r, 'lqr_result.pdf');
+
+%% Imposição de polos com polos de valor grande:
+
+pF = [-5 -2 -5 -2];
+pK = [-1 -2 -1 -2];
+F = -place(A , B, pF);
+% Para pK * 500, os valores do esforço de controle -> inf
+K = place(A.',C.',pK*120)';
+
+
+%% Simulação de imposição de polos com polos grandes:
+
+sim_out3 = sim("comp_model.slx", ...
+    'StartTime', '0', ...
+    'StopTime', num2str(Tf), ...
+    'FixedStep', num2str(sim_step));
+
+comp_plot(sim_out3, r, 'place2_result.pdf');
+
+%% Rastreamento
+
+Ar   = [0      1 0      0 0   0; 
+        -K_m/M 0 K_m/M  0 1/M 0; 
+        0      0 0      1 0   0;
+        K_m/M  0 -K_m/M 0 0   1/M;
+        0      0 0      0 0   0;
+        0      0 0      0 0   0];
+    
+Br   = [0   0   0   0; 
+        1/M 0   0   0; 
+        0   0   0   0; 
+        0   1/M 0   0;
+        0   0   1   0;
+        0   0   0   1];
+
+Cr   = [1 0 0 0 0 0; 
+        0 0 1 0 0 0];
+
+Dr   = [0 0; 0 0; 0 0; 0 0];
+
+Amr = [0 0; 0 0];
+Bmr = [1 0; 0 1];
+Cmr = [1 0; 0 1];
+Dmr = [0 0; 0 0];
+
+x0r = [x0; 0; 0];
+
+Fr = -lqr(Ar,Br,eye(6),eye(4),0);
+Kr = lqr(Ar.',Cr.',eye(6),eye(2),0)';
+
+%% Simulação de sistema aumentado para rastreamento:
+
+sim_out4 = sim("rstr_model.slx", ...
+    'StartTime', '0', ...
+    'StopTime', num2str(Tf), ...
+    'FixedStep', num2str(sim_step));
+
+comp_plot(sim_out4, r, 'rstr_result.pdf');
